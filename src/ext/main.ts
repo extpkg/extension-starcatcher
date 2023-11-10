59f5d59a58eab17f5197349e8ea523e80f6272ff
Bodgan B Peterson <github@bogdanbpeterson.com>
2023-11-10 17:01:27 +0200

    fix: disables maximizable

--- src/ext/main.ts
+++ src/ext/main.ts
@@ -55,7 +55,7 @@ ext.runtime.onExtensionClick.addListener(async () => {
 
     window = await ext.windows.create({
       center: true,
-      fullscreenable: true,
+      maximizable: false,
       title,
       icon: "./assets/128.png",
       darkMode: true,
